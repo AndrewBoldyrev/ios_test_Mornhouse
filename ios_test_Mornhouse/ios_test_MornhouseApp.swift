eab33910709c092b3d96cd469b97dfdc659b0c00
andrewB <boldforgit@gmail.com>
2022-12-20 12:38:59 +0200

    Initial Commit

---
+++ ios_test_Mornhouse/ios_test_MornhouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_test_MornhouseApp.swift
+//  ios_test_Mornhouse
+//
+//  Created by Andrew Boldyrev on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ios_test_MornhouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
